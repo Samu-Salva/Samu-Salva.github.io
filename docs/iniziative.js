7f4490739a6f76f6ba86069bd2618796e1ec6fc2
Samu-Salva <samuel.salvatore2006@gmail.com>
2023-04-01 16:59:21 +0200

    premi

--- docs/iniziative.js
+++ docs/iniziative.js
@@ -1,29 +1,24 @@
-var slideIndex = [1,1, 1];
-var slideId = ["gallery1", "gallery2", "gallery3"];
+var slideIndex = [1,2,3,4];
+var slideId = ["gallery1", "gallery2", "gallery3", "gallery4"];
 
 showSlides(1, 0);
 showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
 }
 
-function currentSlide(n, no) {
-  showSlides(slideIndex[no] = n, no);
-}
-
 function showSlides(n, no) {
   var i;
   var slides = document.getElementById(slideId[no]).getElementsByClassName("mySlides");
-  var dots = document.getElementById(slideId[no]).getElementsByClassName("dot");
   if (n > slides.length) {slideIndex[no] = 1}
   if (n < 1) {slideIndex[no] = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
+
   slides[slideIndex[no]-1].style.display = "block";
   dots[slideIndex[no]-1].className += " active";
 }
